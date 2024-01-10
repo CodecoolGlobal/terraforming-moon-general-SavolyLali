09574f04d4cf71f04fadb35ff5a05d265833b1c8
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-01-10 09:43:13 +0100

    create a security group that allows SSH connection

--- main.tf
+++ main.tf
@@ -32,12 +32,38 @@ resource "aws_key_pair" "this" {
   public_key = file("~/.ssh/id_ALGORITHM.pub")
 }
 
+resource "aws_security_group" "this" {
+  name        = "Allow SSH"
+
+  ingress {
+    description = "SSH from internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 resource "aws_instance" "this" {
   ami           = data.aws_ami.this.id
   instance_type = "t2.micro"
   key_name = aws_key_pair.this.key_name
+  vpc_security_group_ids = [aws_security_group.this.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "tf-moon-sl"
   }
 }
+
